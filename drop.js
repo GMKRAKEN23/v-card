7cc4600ce33c77a61f51e60d94fe735f1cb86062
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-11-29 19:42:55 +0100

    modification HTML and Js

--- drop.js
+++ drop.js
@@ -1,5 +1,15 @@
-let dropdown = document.getElementById('drop');
+let dropdown = document.getElementById('drop'); 
 
 function toogleMenu() {
     dropdown.classList.toggle('active');
+}
+
+
+let button = document.getElementsByClassName('button_style');
+
+function switchColor(couleur) { 
+    for (let i = 0; i < button.length; i++) {
+        button[i].style.backgroundColor = couleur;
+        button[i].style.color = '#fff';
+    }
 }
